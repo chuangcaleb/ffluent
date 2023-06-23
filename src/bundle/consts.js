5b100d11909b4a8eab6bd72c3e2af8dfdcb4066e
chuangcaleb <balechuang@gmail.com>
2023-06-23 18:11:43 +0800

    refac: use nullish coeles assign in resolving srcDir

--- src/bundle/consts.js
+++ src/bundle/consts.js
@@ -4,9 +4,9 @@ const SRC = 'src';
 const OUT = 'out';
 
 export const CONFIG_FILENAME = 'ffluent';
-export const OUT_FILENAME = 'output.fountain';
+export const DEFAULT_OUT_FILENAME = 'output.fountain';
 
 export const CWD = process.cwd();
-export const SRC_DIR = path.resolve(CWD, SRC);
+export const DEFAULT_SRC_DIR = path.resolve(CWD, SRC);
 
-export const OUT_DIR = path.resolve(CWD, OUT);
+export const DEFAULT_OUT_DIR = path.resolve(CWD, OUT);
--- src/bundle/resolveConfig.js
+++ src/bundle/resolveConfig.js
@@ -1,13 +1,19 @@
 import path from 'path';
-import { OUT_DIR, OUT_FILENAME, SRC_DIR } from './consts.js';
+import {
+  DEFAULT_OUT_DIR,
+  DEFAULT_OUT_FILENAME,
+  DEFAULT_SRC_DIR,
+} from './consts.js';
 
 // TODO: handle no config / missing required
 export default function resolveConfig(config) {
+  var { outputDir, outputFilename, title, srcDir } = config;
   const outputFilepath = path.resolve(
-    config?.outputDir ?? OUT_DIR,
-    config?.outputFilename ?? `${config?.title}.fountain` ?? OUT_FILENAME
+    outputDir ?? DEFAULT_OUT_DIR,
+    outputFilename ?? (title && `${title}.fountain`) ?? DEFAULT_OUT_FILENAME
   );
-  const srcDir = config?.srcDir ?? SRC_DIR;
+
+  srcDir ??= DEFAULT_SRC_DIR;
 
   return {
     ...config,

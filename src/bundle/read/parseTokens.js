c3556b2ce5af92646cbe4f8cdaf276d69ef18bd8
chuangcaleb <balechuang@gmail.com>
2023-06-29 12:33:33 +0800

    feat: trim fountain file content

--- src/bundle/read/parseTokens.js
+++ src/bundle/read/parseTokens.js
@@ -3,7 +3,7 @@ import readFile from './readFile.js';
 export default function parseTokens(tokens) {
   return tokens.map((token) => {
     if (token.type === 'text') return token.content;
-    if (token.type === 'file') return readFile(token.content);
+    if (token.type === 'file') return readFile(token.content).trim();
     throw new Error(
       `Invalid token type encountered: "${token.type}" for token content: "${token.content}"`
     );
